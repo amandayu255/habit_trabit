d4d279b0f8dc3c17c06fbebc3bb98f5b8f3c3c99
amandayu255 <amandayu255@gmail.com>
2025-03-18 19:04:57 -0700

    added functionality to edit screen

--- app/src/main/java/com/zybooks/habit/HabitTrackerApp.kt
+++ app/src/main/java/com/zybooks/habit/HabitTrackerApp.kt
@@ -2,30 +2,28 @@ package com.zybooks.habit
 
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.zybooks.habit.viewmodel.HomeViewModel
-import com.zybooks.habit.viewmodel.AddEditHabitViewModel
-import com.zybooks.habit.viewmodel.SettingsViewModel
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.zybooks.habit.ui.AddHabitScreen
 import com.zybooks.habit.ui.EditHabitScreen
 
-
 @Composable
 fun HabitTrackerApp(modifier: Modifier = Modifier) {
     var screen by remember { mutableStateOf("Home") }
-    var selectedHabit by remember { mutableStateOf("") }
+    var selectedHabitIndex by remember { mutableStateOf<Int?>(null) }
 
     val homeViewModel: HomeViewModel = viewModel()
-    val addEditHabitViewModel: AddEditHabitViewModel = viewModel()
-    val settingsViewModel: SettingsViewModel = viewModel()
 
     when (screen) {
-        "Home" -> HomeScreen(homeViewModel) { newScreen, habit ->
-            selectedHabit = habit ?: ""
-            screen = newScreen
+        "Home" -> HomeScreen(homeViewModel) { destination, index ->
+            selectedHabitIndex = index
+            screen = destination
         }
         "AddHabit" -> AddHabitScreen(homeViewModel) { screen = "Home" }
-        "EditHabit" -> EditHabitScreen(addEditHabitViewModel, homeViewModel, selectedHabit) { screen = "Home" }
-        "Settings" -> SettingsScreen(settingsViewModel) { screen = "Home" }
+        "EditHabit" -> {
+            selectedHabitIndex?.let { index ->
+                EditHabitScreen(homeViewModel, index) { screen = "Home" }
+            }
+        }
     }
 }
