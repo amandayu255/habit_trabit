5405c8f9a323349d8dc9793612c4eb025f868149
amandayu255 <amandayu255@gmail.com>
2025-03-18 18:40:16 -0700

    added functionality to add/update habits

--- app/src/main/java/com/zybooks/habit/viewmodel/ViewModels.kt
+++ app/src/main/java/com/zybooks/habit/viewmodel/ViewModels.kt
@@ -8,10 +8,21 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
     var habits = mutableStateListOf("Drink water", "Charge phone", "Bring water bottle")
+
+    fun addHabit(newHabit: String) {
+        habits.add(newHabit)
+    }
+
+    fun updateHabit(oldHabit: String, newHabit: String) {
+        val index = habits.indexOf(oldHabit)
+        if (index != -1) {
+            habits[index] = newHabit
+        }
+    }
 }
 
 class AddEditHabitViewModel : ViewModel() {
-    var habitName by mutableStateOf("")
+    var habitName = mutableStateOf("")
 }
 
 class SettingsViewModel : ViewModel() {
